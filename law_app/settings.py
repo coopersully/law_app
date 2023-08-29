69357a88a016c0528146cd43756e16be411bf762
Cooper Sullivan <thecoopersully@gmail.com>
2023-08-29 12:09:29 -0500

    Add python-dotenv

--- law_app/settings.py
+++ law_app/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
