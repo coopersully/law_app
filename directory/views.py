35db47a669dac052e2c2659a48e9e9c463feaec7
Cooper Sullivan <coopersully02@gmail.com>
2023-09-12 17:19:30 -0500

    Add search bar to directory

--- directory/views.py
+++ directory/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import generic
 
@@ -5,13 +6,22 @@ from accounts.models import CustomUser
 
 
 def directory(request):
-    # Gets the 'role' query parameter, defaults to 'all'
     role_filter = request.GET.get('role', 'all')
+    search_query = request.GET.get('search', '')
 
     if role_filter.lower() == 'all':
-        users = CustomUser.objects.order_by("first_name")
+        users = CustomUser.objects.all()
     else:
-        users = CustomUser.objects.filter(role=role_filter).order_by("first_name")
+        users = CustomUser.objects.filter(role=role_filter)
+
+    if search_query:
+        users = users.filter(
+            Q(first_name__icontains=search_query) |
+            Q(last_name__icontains=search_query) |
+            Q(username__icontains=search_query)
+        )
+
+    users = users.order_by("first_name")
 
     context = {
         'page_name': 'directory',
