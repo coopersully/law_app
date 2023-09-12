0e555a7693180a5f96ad5e50930b426a5c1aff06
Cooper Sullivan <coopersully02@gmail.com>
2023-09-12 14:40:07 -0500

    Add faux user data command
    `python manage.py createfakeusers <int>`
    
    Co-Authored-By: Andrew DeSimone <36091688+AndrewDeSimone@users.noreply.github.com>

--- directory/urls.py
+++ directory/urls.py
@@ -4,5 +4,5 @@ from directory import views
 
 urlpatterns = [
     path('directory/', views.directory, name='directory'),
-    path('user/<str:username>/', views.profile, name='profile'),
+    path('user/<str:username>/', views.profile, name='view_profile'),
 ]
