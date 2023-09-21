0867bdfdd6fbcf5b7096ec281a843bd22074f3e5
Andrew DeSimone <andrewdesimone96@gmail.com>
2023-09-21 13:13:41 -0500

    Add agenda model

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from accounts.models import CustomUser
 # Create your models here.
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    datetime = models.DateTimeField()
+    owner = models.ForeignKey(CustomUser, related_name='events', on_delete=models.CASCADE)
