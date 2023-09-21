d87483e5cd7111a0924d5cff398c043f03aa686e
Andrew DeSimone <andrewdesimone96@gmail.com>
2023-09-21 14:13:06 -0500

    Add announcement model

--- agenda/models.py
+++ agenda/models.py
@@ -9,3 +9,7 @@ class Event(models.Model):
     location = models.CharField(max_length=200)
     datetime = models.DateTimeField()
     owner = models.ForeignKey(CustomUser, related_name='events', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
