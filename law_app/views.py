defc22a36dd2f4e7d00b68e980e97fff2a3ea81e
Cooper Sullivan <thecoopersully@gmail.com>
2023-09-26 18:20:15 -0500

    Flesh out dashboard

--- law_app/views.py
+++ law_app/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from agenda.models import Event
+from announcements.models import Announcement
+
 
 def home(request):
     context = {'page_name': 'home'}
@@ -9,5 +12,9 @@ def home(request):
 
 @login_required
 def dashboard(request):
-    context = {'page_name': 'dashboard'}
+
+    events = Event.objects.all()
+    announcements = Announcement.objects.all()
+
+    context = {'page_name': 'dashboard', 'events': events, 'announcements': announcements}
     return render(request, 'dashboard.html', context)
