59fc38ef993257d863fa30f21513c7c744fcd66a
Cooper Sullivan <coopersully02@gmail.com>
2023-10-08 16:58:38 -0400

    Fix dashboard login issue

--- law_app/settings.py
+++ law_app/settings.py
@@ -152,7 +152,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Account management - redirects
-LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 # Authentication
