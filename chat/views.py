71db90c97557e151d7c49dcd9daa0019b30b5fc7
Cooper Sullivan <thecoopersully@gmail.com>
2023-10-19 16:20:11 -0500

    Revert "Improve message registry"
    This reverts commit 35cf2425437152743d9f7f473e6c790f585bbfef.

--- chat/views.py
+++ chat/views.py
@@ -16,29 +16,9 @@ def chat(request):
 def get_past_messages(request, room_name):
     last_10_messages = Message.objects.filter(room_name=room_name).order_by('-timestamp')[:10]
     messages = [{
-        'id': message.id,
         'content': message.content,
         'author': message.author.username,
         'timestamp': str(message.timestamp)
     } for message in reversed(last_10_messages)]
 
     return JsonResponse({'messages': messages})
-
-
-@login_required
-def register_message(request):
-    message_content = request.POST.get('message')
-    room_name = request.POST.get('room_name')
-    author = request.user
-
-    # Create a new Message object
-    new_message = Message.objects.create(
-        room_name=room_name,
-        author=author,
-        content=message_content
-    )
-
-    # Save the new Message object to the database
-    new_message.save()
-
-    return JsonResponse({'message_id': new_message.id})
