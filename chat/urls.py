b6c7e77bc286d8e1a8f4d9fbc51b127305624089
Cooper Sullivan <thecoopersully@gmail.com>
2023-10-23 17:57:02 -0500

    Revert "Add message registry :one:"
    This reverts commit 154e2f4506d5c67a2be2981587ab10a4ddc97d2e.

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.chat, name='chat'),
     path('<str:room_name>/past_messages/', views.get_past_messages, name='get_past_messages'),
-    path('register_message/', views.register_message, name='register_message')
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,7 @@
-import json
-
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from accounts.models import CustomUser
 from chat.models import Message
 
 
@@ -26,26 +23,3 @@ def get_past_messages(request, room_name):
     } for message in reversed(last_10_messages)]
 
     return JsonResponse({'messages': messages})
-
-
-@login_required
-def register_message(request):
-    data = json.loads(request.body.decode('utf-8'))
-    message_content = data.get('message')
-    room_name = data.get('room_name')
-    author = request.user
-
-    print(f'message_content: {message_content}')
-    print(f'room_name: {room_name}')
-
-    # Create a new Message object
-    new_message = Message.objects.create(
-        room_name=room_name,
-        author=author,
-        content=message_content
-    )
-
-    # Save the message to the database
-    new_message.save()
-
-    return JsonResponse({'message_id': new_message.id})
