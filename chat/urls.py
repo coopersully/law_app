154e2f4506d5c67a2be2981587ab10a4ddc97d2e
Cooper Sullivan <thecoopersully@gmail.com>
2023-10-23 13:36:31 -0500

    Add message registry :one:

--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.chat, name='chat'),
     path('<str:room_name>/past_messages/', views.get_past_messages, name='get_past_messages'),
+    path('register_message/', views.register_message, name='register_message')
 ]
