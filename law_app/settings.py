47986259c6f930965447c8204c49f65de2992669
Cooper Sullivan <thecoopersully@gmail.com>
2023-11-21 13:01:24 -0600

    Update settings.py

--- law_app/settings.py
+++ law_app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
-CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
+CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS").split(",")
 
 #API Keys
 OPEN_WEATHER = os.environ.get("OPEN_WEATHER_API_KEY")
